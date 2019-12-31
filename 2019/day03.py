12dc4e243e9a4c7fab0e4da5b0616c6987162087
mxrten <mxrten@gmail.com>
2019-12-31 23:36:16 +0000

    Day 3

--- 2019/day03.py
+++ 2019/day03.py
@@ -5,21 +5,46 @@ ROOT = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(ROOT)
 from aoc_utils import get_input
 
-#import numpy as np
+import numpy as np
 #from collections import deque
 
 def run(indata):
 	L = indata.splitlines(keepends=False)
 	
+	# Directions
+	D = {
+		'U': (0, 1),
+		'D': (0, -1),
+		'L': (-1, 0),
+		'R': (1, 0)
+	}
+	
+	# Walk every thread
+	threads = []
+	for l in L:
+		walk = np.zeros((0,2), dtype=int)
+		for t in l.split(','):
+			walk = np.vstack((walk, np.tile(D[t[0]], (int(t[1:]), 1))))
+		threads.append(np.cumsum(walk, axis=0))
+	
 	# ----------- PART 1 -----------
 	#
-	ans1 = None
+	T1 = set([tuple(pos) for pos in threads[0]])
+	T2 = set([tuple(pos) for pos in threads[1]])
+	X = T1 & T2
+	ans1 = 9999999
+	for x in X:
+		ans1 = min(abs(x[0])+abs(x[1]), ans1)
 	print("Part 1: {}".format(ans1))
 	clipboard.set("{}".format(ans1))
 	
 	# ----------- PART 2 -----------
 	#
-	ans2 = None
+	T1 = dict([(tuple(pos), s+1) for s, pos in enumerate(threads[0])])
+	T2 = dict([(tuple(pos), s+1) for s, pos in enumerate(threads[1])])
+	ans2 = 9999999
+	for x in X:
+		ans2 = min(T1[x]+T2[x], ans2)
 	print("Part 2: {}".format(ans2))
 	clipboard.set("{}".format(ans2))
 
