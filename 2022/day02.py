c31a5d00de9766fea2aa3239107f5c91d7ff8f31
Mårten Selin <marten.selin@tobii.com>
2022-12-03 06:19:48 +0100

    Day 2 and 3

---
+++ 2022/day02.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import numpy as np
+
+#from collections import deque
+
+def run(indata):
+    L = indata.splitlines(keepends=False)
+    S = np.array([(ord(l[0]) - ord('A') + 1, ord(l[2]) - ord('X') + 1) for l in L])
+    
+    # ----------- PART 1 -----------
+    #
+    answer = np.sum(S[:, 1] + 3 * np.mod(S[:, 1] - S[:, 0] + 1, 3))
+    print("Part 1: {}".format(answer))
+    
+    # ----------- PART 2 -----------
+    #
+    M = np.mod((S[:, 0] - 1) + (S[:, 1] - 2), 3) + 1
+    answer = np.sum(M + 3 * (S[:, 1] - 1))
+    print("Part 2: {}".format(answer))
+    
+
+if __name__ == '__main__':
+    # Initialize
+    ROOT = os.path.dirname(os.path.dirname(__file__))
+    sys.path.append(ROOT)
+    from aoc_utils import get_input, clipboard_set
+
+    # Get input data
+    day = int(os.path.basename(__file__)[3:5])
+    year = int(os.path.basename(os.path.dirname(__file__)))
+    indata = get_input(day, year)
+    
+    # Run
+    answer = run(indata)
+
+    # Copy to clipboard?
+    if answer is not None:
+        clipboard_set(str(answer))
---
+++ 2022/day03.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import numpy as np
+
+#from collections import deque
+
+def run(indata):
+    L = indata.splitlines(keepends=False)
+    
+    # ----------- PART 1 -----------
+    #
+    R = [(set([i for i in l[:(len(l)//2)]]), set([i for i in l[(len(l)//2):]])) for l in L]
+    I = [r[0].intersection(r[1]) for r in R]
+    answer = 0
+    for i in I:
+        assert len(i) == 1
+        for _i in i:
+            answer += ord(_i) - ord('a') + 1 if _i == _i.lower() else ord(_i) - ord('A') + 27
+
+    print("Part 1: {}".format(answer))
+    
+    # ----------- PART 2 -----------
+    #
+    answer = 0
+    for g in range(len(L)//3):
+        r1 = set([i for i in L[3*g + 0]])
+        r2 = set([i for i in L[3*g + 1]])
+        r3 = set([i for i in L[3*g + 2]])
+        for i in r1.intersection(r2).intersection(r3):
+            assert len(i) == 1
+            for _i in i:
+                answer += ord(_i) - ord('a') + 1 if _i == _i.lower() else ord(_i) - ord('A') + 27
+    print("Part 2: {}".format(answer))
+    
+
+if __name__ == '__main__':
+    # Initialize
+    ROOT = os.path.dirname(os.path.dirname(__file__))
+    sys.path.append(ROOT)
+    from aoc_utils import get_input, clipboard_set
+
+    # Get input data
+    day = int(os.path.basename(__file__)[3:5])
+    year = int(os.path.basename(os.path.dirname(__file__)))
+    indata = get_input(day, year)
+    
+    # Run
+    answer = run(indata)
+
+    # Copy to clipboard?
+    if answer is not None:
+        clipboard_set(str(answer))
